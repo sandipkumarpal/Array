634ff7a7b8062d12f7ee3c49b1e25fcb065ac7eb
sandipkumarpal <sandip.pal18@gmail.com>
2020-09-01 15:46:15 +0530

    Array Of

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from "react";
 import "./styles.css";
+
 // 1
-const array = Array.from("Sandip");
-console.log({ array });
-
-// 2
-const set = new Set(["foo", "bar", "baz", "foo"]);
-
-console.log(Array.from(set));
-
-// 3
-
-function f() {
-  return Array.from(arguments);
-}
-console.log(f(1, 2, 3));
-
-// 4
-const mapArr = Array.from([1, 2, 3], (x) => x * x);
-console.log(mapArr);
-
-// 5
-const mapArr2 = Array.from({ length: 5 }, (v, i) => ({
-  [i]: "Sandip " + i
-}));
-console.log(mapArr2);
+console.log(Array.of(1)); // [1]
+console.log(Array.of(1, 2, 3)); // [1, 2, 3]
+console.log(Array.of(undefined)); // [undefined]4
+console.log(Array(1));
 
 export default function App() {
   return (
